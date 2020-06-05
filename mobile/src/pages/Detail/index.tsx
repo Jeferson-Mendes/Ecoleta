a607144221da3374c81dbd926d04bff4c8046f09
Jeferson-Mendes <jefersonmendes3c@gmail.com>
2020-06-05 11:11:24 -0300

    configurando upload

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -16,6 +16,7 @@ interface Params {
 interface Data {
     point: {
         image: string;
+	img_url: string;
         name: string;
         email: string;
         whatsapp: string;
@@ -67,7 +68,7 @@ const Detail = () => {
                 <Icon name="arrow-left" size={20} color="#34cb79" />
             </TouchableOpacity>
 
-            <Image style={styles.pointImage} source={{ uri: data.point.image }} />
+            <Image style={styles.pointImage} source={{ uri: data.point.img_url }} />
             
             <Text style={styles.pointName} >{data.point.name}</Text>
             <Text style={styles.pointItems} >
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import PointController from './controllers/PointsController';
 import ItemController from './controllers/ItemsController';
 
@@ -7,11 +10,14 @@ const pointController = new PointController()
 const itemController = new ItemController()
 
 const routes = express.Router();
+const upload = multer(multerConfig)
 
 routes.get('/items', itemController.index);
-
 routes.get('/points', pointController.index);
-routes.post('/points', pointController.create);
+
+routes.post('/points' , upload.single('image') , pointController.create);
+
+
 routes.get('/points/:id', pointController.show);
 
 export default routes;
